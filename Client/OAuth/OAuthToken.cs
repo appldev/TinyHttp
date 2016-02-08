babe840781fe8ff81c43d2a4ab88dea16b73d94d
Michael Randrup <mra@applications.dk>
2016-02-08 09:21:08 +0100

    added nuget specification. v 0.1.0 uploaded to nuget.org

---
+++ Client/OAuth/OAuthToken.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace TinyHttp
+{
+    public class OAuthAccessToken : OAuthTokenBase
+    {
+
+    }
+}
---
+++ Client/nuspec/build-nuget-package.bat
@@ -0,0 +1,2 @@
+nuget.exe pack TinyHttp.nuspec -NonInteractive -Verbosity quiet
+Ver > nul
